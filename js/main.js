94d397512aec51f3ff92a2acce53b85d56402a91
0pashich <0pashich@gmail.com>
2021-06-01 03:16:16 +0300

    make homework for lesson 2

--- js/main.js
+++ js/main.js
@@ -1,22 +1,70 @@
-const products = [
-    { id: 1, title: 'Notebook', price: 2000 },
-    { id: 2, title: 'Mouse', price: 20 },
-    { id: 3, title: 'Keyboard', price: 200 },
-    { id: 4, title: 'Gamepad', price: 50 },
-    { id: 5, title: 'Monitor' },
-];
-//Функция для формирования верстки каждого товара
-const renderProduct = (title, price = 'по звонку') =>
-    `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
+class GoodsItem {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
+    render(img = 'https://via.placeholder.com/200x150') {
+        return `<div class="product-item">
+                <img src="${img}">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
-    ;
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price));
-    console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList.join('');
-};
+    }
+}
 
-renderPage(products);
+
+class GoodsList {
+    constructor() {
+        this.goods = [];
+    }
+    fetchGoods() {
+        this.goods = [
+            { id: 1, title: 'Notebook', price: 2000 },
+            { id: 2, title: 'Mouse', price: 20 },
+            { id: 3, title: 'Keyboard', price: 200 },
+            { id: 4, title: 'Gamepad', price: 50 },
+        ];
+    }
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.title, good.price);
+            listHtml += goodItem.render();
+        });
+        document.querySelector('.products').innerHTML = listHtml;
+    }
+    getSum() {
+        let totalPrice = 0;
+        this.goods.forEach(good => totalPrice += good.price);
+        return totalPrice;
+    }
+}
+
+class CartItem {
+    constructor(title, price, quantity) {
+        this.title = title;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    render() { }
+    setQuantity() { }
+}
+
+class CartList {
+    constructor() {
+        this.goods = [];
+    }
+    fetchCart() { }
+    render() { }
+    add() { }
+    remove() { }
+    buy() { }
+    clear() { }
+    getSum() { }
+}
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+console.log(list.getSum());
