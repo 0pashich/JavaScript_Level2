187d898152f6926707a2e84c89a23f4efc64cb64
0pashich <0pashich@gmail.com>
2021-06-16 23:07:31 +0300

    update

--- js/SearchComponent.js
+++ js/SearchComponent.js
@@ -1,13 +1,8 @@
 
 Vue.component('search', {
-    data() {
-        return {
-            userSearch: ''
-        }
-    },
     template: `
-    <form action="#" class="search-form" @submit.prevent="$parent.filter(userSearch)">
-        <input type="text" class="search-field" v-model='userSearch'>
+    <form action="#" class="search-form" @submit.prevent="$parent.filter($parent.userSearch)">
+        <input type="text" class="search-field" v-model='$parent.userSearch'>
         <button type="submit" class="btn-search">
             <i class="fas fa-search"> </i>
             </button>
